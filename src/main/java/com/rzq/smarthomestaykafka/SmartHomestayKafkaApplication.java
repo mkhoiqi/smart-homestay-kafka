6f194a6d01c33e9201e1368ce6ec962a5c216813
mkhoiqi <mkhoirurrizqi08@gmail.com>
2023-08-24 14:47:47 +0700

    init commit

---
+++ src/main/java/com/rzq/smarthomestaykafka/SmartHomestayKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.rzq.smarthomestaykafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomestayKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomestayKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rzq/smarthomestaykafka/SmartHomestayKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rzq.smarthomestaykafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomestayKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
